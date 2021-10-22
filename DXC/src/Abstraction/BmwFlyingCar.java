163a0f3a0170222f08a261e2f0e4d01f3c85326c
kasthuri <kasthuri@Asus>
2021-10-22 14:29:05 +0300

    polymorphism

---
+++ DXC/src/Abstraction/BmwFlyingCar.java
@@ -0,0 +1,17 @@
+package Abstraction;
+
+public class BmwFlyingCar extends FlyingCar {
+	@Override
+	public void stopCar() {
+		//TODO Auto-generate dmethod stub
+	}
+	@Override
+	public void flyCar() {
+		//TODO Auto-generated method stub
+	}
+	@Override
+	public void landCar() {
+		//TODO Auto-generated method stub
+	}
+
+}
---
+++ DXC/src/Polymorphism/Animal.java
@@ -0,0 +1,12 @@
+package Polymorphism;
+
+public class Animal {
+	Animal(){System.out.println("animal is created");}
+}
+class Dog extends Animal{
+	Dog(){
+		//super();
+		System.out.println("dog is created");
+	}
+
+}
