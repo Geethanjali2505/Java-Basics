3c599e6bdf98c0c95e72879455253938b8ed9360
kasthuri <kasthuri@Asus>
2021-10-25 13:10:26 +0300

    Threads.sync

--- DXC/src/Javastrings/Solution1.java
+++ DXC/src/Javastrings/Solution1.java
@@ -1,5 +1,13 @@
 package Javastrings;
 
-public class Solution1 {
+import java.util.Scanner;
 
+public class Solution1 {public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    String S = in.next();
+    int start = in.nextInt();
+    int end = in.nextInt();
 }
+}
+
+
