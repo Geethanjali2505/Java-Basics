2796633cd8e3b629eb32acd969352c45355972df
kasthuri <kasthuri@Asus>
2021-10-26 10:20:49 +0300

    collections

---
+++ DXC/src/Collections/DemoArrayList.java
@@ -0,0 +1,24 @@
+package Collections;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class DemoArrayList {
+	public static void main(String[] args) {
+		List<String> names = new ArrayList<String>();
+		names.add("abdul");
+		names.add("ansari");
+
+		//names.add(30);
+		System.out.println(names);
+		/*
+		 * for(String name : names) { //for each name in names
+		 * System.out.println("the name is "+ name); }
+		 */
+
+		Iterator itr=names.iterator();  
+		while(itr.hasNext()){  
+		System.out.println(itr.next());  
+		}  
+	}
+}
